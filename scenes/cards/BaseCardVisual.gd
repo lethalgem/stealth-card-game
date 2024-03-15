175a7ec740ee30b14a066c5d3a6ef6bc1001db7f
lethalgem <iwc123@gmail.com>
2024-03-15 17:12:57 -0500

    Show card on hover

--- scenes/cards/BaseCardVisual.gd
+++ scenes/cards/BaseCardVisual.gd
@@ -4,5 +4,28 @@ extends Node2D
 
 @export var cardTitle: Label
 
+var is_hovered: bool = false
+var starting_z_index: int
+
+
+func _ready():
+	starting_z_index = z_index
+
+
+func _process(delta):
+	if is_hovered:
+		z_index = 100
+	else:
+		z_index = 0
+
+
 func applyTitle(title: String):
 	cardTitle.text = title
+
+
+func _on_area_2d_mouse_entered():
+	is_hovered = true
+
+
+func _on_area_2d_mouse_exited():
+	is_hovered = false
--- scenes/game/Game.gd
+++ scenes/game/Game.gd
@@ -6,11 +6,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	#cardDeck = CardDeck.new()	
+
+	#cardDeck = CardDeck.new()
 	#hand = Hand.new()
 	cardDeck.createDeck(20)
-	
+
 	for i in range(7):
 		await get_tree().create_timer(.75).timeout
 		draw()
