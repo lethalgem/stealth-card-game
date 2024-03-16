236382d731e997e9e3e9f924f31ebaa04e6f27b4
SuperMB <littlebass09@tamu.edu>
2024-03-16 09:19:39 -0700

    MB Commmitting before pulling

--- scenes/characters/BadShorty.gd
+++ scenes/characters/BadShorty.gd
@@ -1,11 +1,43 @@
+class_name BasShorty
+
 extends Sprite2D
 
+enum DirectionState { left, right, up, down }
+var directionState = DirectionState.left
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#walkLeft()
 	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if directionState == DirectionState.left:
+		walkLeft(delta)
+	elif directionState == DirectionState.right:
+		walkRight(delta)
+	
+	#pass
+
+func walkLeft(tweenTime):
+	flip_h = true
+	$AnimationPlayer.play('walk')
+	#var tweenTime = 2
+	var tween = create_tween()
+	tween.tween_property(self, 'position', Vector2(position.x - 50 * tweenTime , position.y), tweenTime)
+	
+	if position.x < -200:
+		directionState = DirectionState.right
+	
+	
+func walkRight(tweenTime):
+	flip_h = false
+	$AnimationPlayer.play('walk')
+	#var tweenTime = 2
+	var tween = create_tween()
+	tween.tween_property(self, 'position', Vector2(position.x + 50 * tweenTime , position.y), tweenTime)
+
+	if position.x > 200:
+		directionState = DirectionState.left
